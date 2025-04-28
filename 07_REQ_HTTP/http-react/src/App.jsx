3785625eda54a5f95d793cd7474d15933bf50ee4
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-04-28 15:42:38 -0300

    post de dados

--- 07_REQ_HTTP/http-react/src/App.jsx
+++ 07_REQ_HTTP/http-react/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 function App() {
   const url = "http://localhost:3001/products"
   const [products, setProducts] = useState([])
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState(0.0)
   
   // Resgatar dados
   // useeffect executa 4 vezes ;-;
@@ -16,6 +18,24 @@ function App() {
     fetchData()
   }, [])
   console.log(products)
+
+  const handleSubmit = async (form) => {
+    form.preventDefault()
+    const product = {
+      price,
+      name
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+
+
+  }
 
   return (
     <div className="App">
@@ -31,6 +51,17 @@ function App() {
           </ul>
         )
       }
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome: <input type="text" value={name} name="name" id="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço: <input type="number" value={price} name="preco" id="preco" step="0.01" min="0.0" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <button>Sub</button>
+        </form>
+      </div>
     </div>
   );
 }
