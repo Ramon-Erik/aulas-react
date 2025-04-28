b61aec52646c6d07c434863b5f1f06706a5278af
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-04-28 15:15:02 -0300

    receber dados e listar

--- 07_REQ_HTTP/http-react/src/App.jsx
+++ 07_REQ_HTTP/http-react/src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const url = "http://localhost:3001/products"
+  const [products, setProducts] = useState([])
+  
+  // Resgatar dados
+  // useeffect executa 4 vezes ;-;
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+      const data = await res.json()
+      setProducts(data)
+    }
+    fetchData()
+  }, [])
+  console.log(products)
+
   return (
-    <>
+    <div className="App">
       <h1>Oi</h1>
-    </>
-  )
+      {
+        products.length >= 3 && (
+          <ul>
+            {
+              products.map((product) => (
+                <li key={product.id}>{product.name}</li>
+              ))
+            }
+          </ul>
+        )
+      }
+    </div>
+  );
 }
 
-export default App
+export default App;
