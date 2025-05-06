2e85e589d7972a77f49b8ddeceec8ba9247e4c40
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-05-06 16:27:22 -0300

    context

--- 09_CONTEXT/context/src/pages/home/Home.jsx
+++ 09_CONTEXT/context/src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import { CounterContext } from "../../context/CounterContext";
 const Home = () => {
+  const { counter } = useContext(CounterContext);
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      Home
+      <p>value: {counter}</p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
