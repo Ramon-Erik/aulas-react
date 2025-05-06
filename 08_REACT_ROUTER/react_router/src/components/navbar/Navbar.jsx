317183d1743768aa80e6fe3bb58d41f95a33a3a6
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-05-05 21:22:46 -0300

    nav

--- 08_REACT_ROUTER/react_router/src/components/navbar/Navbar.jsx
+++ 08_REACT_ROUTER/react_router/src/components/navbar/Navbar.jsx
@@ -1,17 +1,32 @@
-import { Link } from "react-router-dom"
-import './Navbar.css'
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //       <Link to="/about">About</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      {/* <NavLinkv></NavLinkv> */}
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "active" : "nao-ativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) => (isActive ? "active" : "nao-ativo")}
+      >
+        About
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
