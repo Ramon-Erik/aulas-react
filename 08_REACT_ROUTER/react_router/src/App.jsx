4b5815d35302a4c0546ffebc6d13f966f0643fdf
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-05-05 21:23:32 -0300

    Update App.jsx

--- 08_REACT_ROUTER/react_router/src/App.jsx
+++ 08_REACT_ROUTER/react_router/src/App.jsx
@@ -1,27 +1,32 @@
-import { useState } from "react";
 import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home/'
-import About from './pages/about/About/'
-import Product from './pages/product/Product/'
-import Info from './pages/info/Info/'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home/";
+import About from "./pages/about/About/";
+import Product from "./pages/product/Product/";
+import Info from "./pages/info/Info/";
+import NotFound from "./pages/not-found/NotFound/";
+import SearchForm from "./pages/search-form/SearchForm/";
+import Search from "./pages/search/Search/";
 
 function App() {
-
   return (
     <>
       <header>
         <h1>React router</h1>
       </header>
       <BrowserRouter>
-        <Navbar></Navbar>
+        <Navbar />
+        <SearchForm />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/product/:id/info" element={<Info/>} />
-          <Route path="/product/:id" element={<Product/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product/:id/info" element={<Info />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/search/" element={<Search />} />
+          <Route path="/company/" element={Navigate("/about")} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
