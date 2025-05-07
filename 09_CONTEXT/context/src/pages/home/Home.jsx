fa58f0d304e36b88c0aa046524fd8772fbac2ded
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-05-07 18:39:13 -0300

    updating counter

--- 09_CONTEXT/context/src/pages/home/Home.jsx
+++ 09_CONTEXT/context/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import { useContext } from "react";
 import { CounterContext } from "../../context/CounterContext";
+import ChangeCounterContext from "../../components/change-counter-context/ChangeCounterContext";
+
 const Home = () => {
   const { counter } = useContext(CounterContext);
   return (
     <div>
       Home
       <p>value: {counter}</p>
+      <ChangeCounterContext />
     </div>
   );
 };
