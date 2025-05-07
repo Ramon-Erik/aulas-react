a04711a03af002203c1fc817a38ffdbe8576f855
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-05-07 18:38:59 -0300

    change counter as a hook

--- 09_CONTEXT/context/src/pages/about/About.jsx
+++ 09_CONTEXT/context/src/pages/about/About.jsx
@@ -1,7 +1,15 @@
-const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+import { useCounterContext } from "../../hook/useCounterContext";
+import ChangeCounterContext  from "../../components/change-counter-context/ChangeCounterContext";
 
-export default About
+const About = () => {
+  const { counter } = useCounterContext();
+  return (
+    <div>
+      <p>About</p>
+      <p>count {counter}</p>
+      <ChangeCounterContext />
+    </div>
+  );
+};
+
+export default About;
